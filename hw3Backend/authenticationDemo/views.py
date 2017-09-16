576d63cfabb794d5e1b0ee44c219a27947625927
Alec Lam <alechunterlam@gmail.com>
2017-09-16 18:54:46 -0400

    working on users

--- hw3Backend/authenticationDemo/views.py
+++ hw3Backend/authenticationDemo/views.py
@@ -3,11 +3,20 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.views.generic import TemplateView
+from .models import *
 import math
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 class LoginView(TemplateView):
     def get(self,request):
-        return render(request, 'authenticationDemo/index.html')
+        # user = User.objects.create_user('myusername', 'myemail@crazymail.com', 'mypassword')
+        # user2 = User.objects.create_user('myusername2', 'myemai2@crazymail.com', 'mypassword2')
+
+        users = User.objects.all
+        return render(request, 'authenticationDemo/index.html', {"users": users})
     def post(self,request):
+        username = request.POST["username"]
+        password = request.POST["password"]
         return render(request,"authenticationDemo/index.html")
