6a1664888c0eeb375eff972a0df9a8f9aa122d97
Alec Lam <alechunterlam@gmail.com>
2017-09-16 18:07:40 -0400

    setting up site

--- hw3Backend/authenticationDemo/views.py
+++ hw3Backend/authenticationDemo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+import math
 
 # Create your views here.
+class LoginView(generic.View):
+    def get(self,request):
+        return render(request, 'authenticationDemo/index.html')
+    def post(self,request):
+        return render(request,"authenticationDemo/index.html")
--- hw3Backend/hw3Backend/urls.py
+++ hw3Backend/hw3Backend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url, include
+from authenticationDemo import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.LoginView.as_view(),name='login'),
 ]
