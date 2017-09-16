0e2ae4864c7f90c466f7826e42c8e17b200bd406
Alec Lam <alechunterlam@gmail.com>
2017-09-16 19:46:47 -0400

    added login/registration

--- hw3Backend/authenticationDemo/views.py
+++ hw3Backend/authenticationDemo/views.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from .models import *
 import math
 
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -17,8 +18,16 @@ class LoginView(TemplateView):
     def post(self,request):
         username = request.POST["username"]
         password = request.POST["password"]
-        email = request.POST["email"]
-        return render(request,"authenticationDemo/index.html")
+        user = authenticate(request, username=username, password=password)
+
+        if username == None or password == None or user == None:
+            errorMessage = 'Invalid Credentials.'
+            return render(request,"authenticationDemo/index.html",{"error":errorMessage})
+        if user.is_authenticated():
+            return render(request,"authenticationDemo/profile.html",{"user":user})
+        else:
+            errorMessage = 'Invalid Credentials.'
+            return render(request,"authenticationDemo/index.html",{"error":errorMessage})
 
 
 # Create your views here.
@@ -26,7 +35,15 @@ class RegisterView(TemplateView):
     def get(self,request):
         return render(request, 'authenticationDemo/register.html')
     def post(self,request):
+        users = User.objects.all
         username = request.POST["username"]
         password = request.POST["password"]
         email = request.POST["email"]
-        return render(request,"authenticationDemo/register.html")
+        if username == None or password == None or email == None:
+            errorMessage = 'Invalid Input for User Credentials.'
+            return render(request,"authenticationDemo/register.html",{"error":errorMessage})
+
+        user = User.objects.create_user(username, email, password)
+
+
+        return render(request,"authenticationDemo/index.html", {"users":users})
